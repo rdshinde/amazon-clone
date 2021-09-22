c47b57846bea57be531865526d1a8c809a952832
Rishikesh Shinde <rdshinde2000@gmail.com>
2021-09-22 19:52:51 +0530

    upto db is not defined

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+
 function getItems(){
-    db.collection("items").get().then((querySnapshot)=>{
-        console.log(querySnapshot);
-        querySnapshot.forEach((doc)=>{
-            console.log(`${doc.data()}`);
+    db.collection("items").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
         });
-    
-})
+    });
 }
 getItems();
