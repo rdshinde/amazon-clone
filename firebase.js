bb20fef7f859bf9890c95534ef1e4943da998f49
Rishikesh Shinde <rdshinde2000@gmail.com>
2021-09-23 18:06:33 +0530

    firebase done

--- firebase.js
+++ firebase.js
@@ -1,17 +1,28 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+
+import { getFirestore, collection, getDocs  } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyDkE8fDr-ScIB4kKXWVuem-1JPA_NzBXAY",
-authDomain: "clone-c7def.firebaseapp.com",
-projectId: "clone-c7def",
-storageBucket: "clone-c7def.appspot.com",
-messagingSenderId: "385018069116",
-appId: "1:385018069116:web:a80bcec2a416232fe0c9f9"
+  apiKey: "AIzaSyBF4Pq9u5CeeOfTDkvn85S_yJdqChklpwI",
+  authDomain: "clone-6accd.firebaseapp.com",
+  projectId: "clone-6accd",
+  storageBucket: "clone-6accd.appspot.com",
+  messagingSenderId: "548244516739",
+  appId: "1:548244516739:web:0a27e703f651ec67ca90b6",
+  measurementId: "G-RC5QFBWCHG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // var db = firebase.firestore();
-var db = getFirestore(app);
+const db = getFirestore();
+export{db};
--- script.js
+++ script.js
@@ -1,13 +1,72 @@
+// import { appendFile } from "fs";
+import {
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 
+import { db } from "./firebase.js";
 
-function getItems() {
-    db.collection("items")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`);
-        });
-      });
-      
-  }
-  getItems();
+// import { collection, getDocs } from "firebase/firestore";
+
+const querySnapshot = await getDocs(collection(db, "items"));
+
+let items = [];
+querySnapshot.forEach((doc) => {
+  items.push({
+    id: doc.id,
+    image: doc.data().image,
+    name: doc.data().name,
+    make: doc.data().make,
+    rating: doc.data().rating,
+    price: doc.data().price,
+  });
+  // console.log(`${doc.id} => ${doc.data()}`);
+});
+generateItems(items);
+// console.log(items);
+
+function generateItems(items) {
+  let itemsHTML = "";
+  items.forEach((item) => {
+    let doc = document.createElement("div");
+    doc.classList.add("main-product","m-5");
+    doc.innerHTML = `
+    <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
+                      <!-- image -->
+                      <img class="w-full h-full object-contain" src="${item.image}" alt="">
+                  </div>
+                  <div class="product-name text-gray-700 font-bold text-sm mt-2">
+                    ${item.name}
+                  </div>
+                  <div class="product-make text-green-700">
+                  ${item.make}
+                  </div>
+                  <div class="product-rating text-yellow-500 font-bold my-1">
+                    ⭐️⭐️⭐️⭐️⭐️ ${item.rating}
+                  </div>
+                  <div class="product-price font-bold text-gray-700 text-lg">
+                    ₹ ${item.price}
+                  </div>
+                  <!-- Add to cart button -->
+                  <div class="add-to-cart h-8 w-28 bg-yellow-500 flex items-center justify-center rounded cursor-pointer text-md hover:bg-yellow-600 mt-1 text-white">Add to Cart</div>
+    `
+    let addToCartEl = document.createElement("div");
+    addToCartEl.classList.add("add-to-cart","h-8","w-28","bg-yellow-500","flex","items-center","justify-center","rounded","cursor-pointer",)
+
+  });
+  document.querySelector(".main-section-products").innerHTML = itemsHTML;
+
+}
+
+// function addItems(){
+//   const docRef =  addDoc(collection(db, "items"), {
+//     image: "Ada",
+//     name: "Lovelace",
+//     price: 1815,
+//     rating:5,
+//     make:"appendFile"
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// }
+// addItems();
